fc1094a2bd02b6aa718b6edf054b43d201ff9d60
聂海锋 <niexiaofei@yahoo.com>
2021-01-10 20:40:31 +0800

    feat: 增加 ResizeObserve 引用

--- .umirc.ts
+++ .umirc.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'hooks',
   mode: 'site',
-  // history: {
-  //   type: 'hash',
-  // },
+  history: {
+    type: 'hash',
+  },
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo:
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { default as useInterval } from './useInterval';
 export { default as useTimeout } from './useTimeout';
 export { default as useQuery } from './useQuery';
 export { default as useUpdate } from './useUpdate';
+export { default as useObserver } from './useObserver';
+export { default as useObserverRect } from './useObserverRect';
